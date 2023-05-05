4766e0df6e32b430f3f4ed2420bf9550cfde0550
Yeattt <nekz.8896@gmail.com>
2023-05-05 14:52:04 -0500

    Refactor: code cleaned

--- src/api/middlewares/validate-auth.js
+++ src/api/middlewares/validate-auth.js
@@ -1,30 +1,30 @@
 const jwt = require('jsonwebtoken');
 
 const validateAuth = (req, res, next) => {
-    const token = req.header('x-token');
+   const token = req.header('x-token');
 
-    if (!token) {
-        return res.status(401).json({
-            ok: false,
-            err: 'You are not authorized to do this'
-        });
-    }
+   if (!token) {
+      return res.status(401).json({
+         ok: false,
+         err: 'You are not authorized to do this'
+      });
+   }
 
-    try {
-        const { uid } = jwt.verify(token, process.env.SECRET_KEY);
+   try {
+      const { uid } = jwt.verify(token, process.env.SECRET_KEY);
 
-        req.uid = uid;
+      req.uid = uid;
 
-        next();
-    } catch (error) {
-        console.log(error);
-        return res.stats(401).json({
-            ok: false,
-            err: 'Invalid token'
-        });
-    }
+      next();
+   } catch (error) {
+      console.log(error);
+      return res.stats(401).json({
+         ok: false,
+         err: 'Invalid token'
+      });
+   }
 }
 
 module.exports = {
-    validateAuth
+   validateAuth
 };
