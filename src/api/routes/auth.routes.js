392ecafe2b794ffae94b807e7c13a4d473e83054
Yeattt <nekz.8896@gmail.com>
2023-05-15 21:15:32 -0500

    Feat: auth fields validations

--- src/api/routes/auth.routes.js
+++ src/api/routes/auth.routes.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 
+const { check } = require('express-validator');
+const { validateFields } = require('../middlewares/validate-fields');
+
 const { signUp, signIn } = require('../controllers/auth.controller');
 
-router.post('/signup', signUp);
+router.post('/signup', [
+   check('email', 'Invalid email format').isEmail(),
+   check('contrasena', 'The password must be bigger than 4 digits').isLength({ min: 4 }),
+   validateFields
+], signUp);
 router.post('/signin', signIn);
-// router.post('/signin', signIn);
 
 module.exports = router;
