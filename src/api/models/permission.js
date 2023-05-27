460ca9bc29b4a83b64afcccd141d328c3020f9a7
Yeattt <nekz.8896@gmail.com>
2023-05-27 17:39:17 -0500

    Fix: permission model bugs fixed

--- src/api/models/permission.js
+++ src/api/models/permission.js
@@ -1,10 +1,19 @@
 const { DataTypes } = require('sequelize');
 const db = require('../../config/db');
 
+const Role = require('./role');
+
 const Permission = db.define('permisos', {
+   id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+   },
    nombre: {
       type: DataTypes.STRING
    }
 });
+
+// Permission.belongsToMany(Role, { through: 'Roles_Permissions' });
 
 module.exports = Permission;
