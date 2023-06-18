c07a400cfa0d3d010f13cc7df4361df2b1eef985
Yeattt <nekz.8896@gmail.com>
2023-06-18 11:40:59 -0500

    Feat: validate token route added

--- src/api/routes/auth.routes.js
+++ src/api/routes/auth.routes.js
@@ -3,13 +3,17 @@ const router = require('express').Router();
 const { check } = require('express-validator');
 const { validateFields } = require('../middlewares/validate-fields');
 
-const { signUp, signIn } = require('../controllers/auth.controller');
+const { signUp, signIn, revalidateToken } = require('../controllers/auth.controller');
+const { validateAuth } = require('../middlewares/validate-auth');
 
 router.post('/signup', [
    check('email', 'Invalid email format').isEmail(),
    check('contrasena', 'The password must be bigger than 4 digits').isLength({ min: 4 }),
    validateFields
 ], signUp);
+
 router.post('/signin', signIn);
+
+router.get('/renew', validateAuth, revalidateToken);
 
 module.exports = router;
