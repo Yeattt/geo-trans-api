c82ddb2ba84ab7d71ffdbbb2b3d86a8f100d1671
Yeattt <nekz.8896@gmail.com>
2023-06-19 10:58:59 -0500

    Feat: activate user registration added to the endpoint routes

--- src/api/routes/user.routes.js
+++ src/api/routes/user.routes.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
 
-const { getUsers, getOneUser, createUser, updateUser, deleteUser } = require('../controllers/user.controller');
+const { activateUser, getUsers, getOneUser, createUser, updateUser, deleteUser } = require('../controllers/user.controller');
 
 router.get('/', getUsers);
+
 router.get('/:id', getOneUser);
+
 router.post('/create', createUser);
+
 router.put('/update/:id', updateUser);
+
+router.put('/allow/:id', activateUser);
+
 router.delete('/delete/:id', deleteUser);
 
 module.exports = router;
