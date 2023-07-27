305f7eb03b53e7719f030351eea6f21ff5f19ec8
kevyn108 <kevynrueda108@gmail.com>
2023-07-27 10:17:56 -0500

    feat: :art: lines are commented out in the quotes path

--- src/api/routes/quote.routes.js
+++ src/api/routes/quote.routes.js
@@ -34,21 +34,22 @@ router.post('/create', createQuote);
 //     check('userId', 'The field is required').notEmpty(),
 //     validateFields
 // ], createQuote);
-router.put('/update/:id', [
-    check('codigoCotizacion', 'The field only receives numeric values.').not().isNumeric(),
-    check('cantidad', 'The field only receives numeric values.').not().isNumeric(),
-    check('codigoProducto', 'The field only receives lines of text').not().isString(),
-    check('destino', 'The field only admits lines of text').not().isString,
-    check('empaque', 'The field only admits lines of text').not().isString,
-    check('naturaleza', 'The field only admits lines of text').not().isString,
-    check('numeroRemesa', 'The field only admits lines of text').not().isString,
-    check('origen', 'The field only admits lines of text').not().isString,
-    check('productoTransportar', 'The field only admits lines of text').not().isString,
-    check('saldoPagar', 'The field only accepts numeric values').not().isNumeric,
-    check('unidadMedida', 'The field only accepts numeric values').not().isNumeric,
-    check('valorPagar', 'The field only accepts numeric values').not().isNumeric,
-    validateFields
-], updateQuote);
+router.put('/update/:id', updateQuote);
+// [
+//     check('codigoCotizacion', 'The field only receives numeric values.').not().isNumeric(),
+//     check('cantidad', 'The field only receives numeric values.').not().isNumeric(),
+//     check('codigoProducto', 'The field only receives lines of text').not().isString(),
+//     check('destino', 'The field only admits lines of text').not().isString,
+//     check('empaque', 'The field only admits lines of text').not().isString,
+//     check('naturaleza', 'The field only admits lines of text').not().isString,
+//     check('numeroRemesa', 'The field only admits lines of text').not().isString,
+//     check('origen', 'The field only admits lines of text').not().isString,
+//     check('productoTransportar', 'The field only admits lines of text').not().isString,
+//     check('saldoPagar', 'The field only accepts numeric values').not().isNumeric,
+//     check('unidadMedida', 'The field only accepts numeric values').not().isNumeric,
+//     check('valorPagar', 'The field only accepts numeric values').not().isNumeric,
+//     validateFields
+// ],
 router.delete('/delete/:id', deleteQuote);
 
 module.exports = router;
