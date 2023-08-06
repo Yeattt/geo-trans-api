7b41110bdeb8a54851f74599f973b56ea7517306
Kevyn108 <kevynrueda108@gmail.com>
2023-08-06 15:55:28 -0500

    feat: :construction: I made some routes (requirements to create something)

--- src/api/routes/permission.routes.js
+++ src/api/routes/permission.routes.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
 
+const { check } = require('express-validator');
 const { getPermissions, getOnePermission, createPermission, updatePermission, deletePermission } = require('../controllers/permission.controller');
 
 router.get('/', getPermissions);
 
 router.get('/:nombre', getOnePermission);
 
-router.post('/create', createPermission);
+router.post('/create', [
+    check('nombre', 'The name field is required').notEmpty(),
+    check('nombre', 'The name field is String').isString()
+],createPermission);
 
 router.put('/update/:id', updatePermission);
 
--- src/api/routes/vehicle-type.routes.js
+++ src/api/routes/vehicle-type.routes.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
 
+const { check } = require('express-validator');
 const { createVehicleType, getOneVehicleType, getVehiclesType, updateVehicleType, deleteVehicleType } = require('../controllers/vehicles-type.controller');
 
 router.get('/', getVehiclesType);
 
 router.get('/:nombre', getOneVehicleType);
 
-router.post('/create', createVehicleType);
+router.post('/create',[
+    check('nombre','The name field is required').notEmpty(),
+    check('nombre', 'The name field is String').isString()
+] ,createVehicleType);
 
 router.put('/update/:id', updateVehicleType);
 
