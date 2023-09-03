a57bf4646a080757e0eb5cca81dadd5ff9b121c5
Yeattt <nekz.8896@gmail.com>
2023-09-03 13:46:42 -0500

    Fix: seed endpoint fixed

--- src/api/controllers/seed.controller.js
+++ src/api/controllers/seed.controller.js
@@ -3,15 +3,19 @@ const bcrypt = require('bcryptjs');
 const VehiclesType = require('../models/vehicle-type');
 const Vehicle = require('../models/vehicles');
 const Permission = require('../models/permission');
+const Privilege = require('../models/privilege');
 const Role = require('../models/role');
 const Company = require('../models/company');
 const User = require('../models/user');
+const Client = require('../models/client');
 
 const seedDB = async(req, res) => {
     try {
         const vehicleType = await VehiclesType.create({
             nombre: "Gasolina"
         });
+
+        await vehicleType.save();
 
         const vehicle = await Vehicle.create({
             modelo: 2019,
@@ -22,25 +26,62 @@ const seedDB = async(req, res) => {
             tecnomecanica: "si",
             soat: "si",
             tipoCamion: 1
-        }, {
-            modelo: 2019,
-            marca: "Mercedez",
-            placa: "ZIO341",
-            placaSemirremolque: "AZD231",
-            tarjetaPropiedad: "si",
-            tecnomecanica: "si",
-            soat: "si",
-            tipoCamion: 1
         });
 
-        const permission = await Permission.create({
-            nombre: "Actualizar"
-        });
+        await vehicle.save();
+
+        const privileges = await Privilege.bulkCreate([
+            {
+                nombre: 'actualizar',
+            },
+            {
+                nombre: 'crear',
+            },
+            {
+                nombre: 'eliminar',
+            },
+        ]);
+
+        const permissions = await Permission.bulkCreate([
+            {
+                nombre: 'usuarios',
+            },
+            {
+                nombre: 'vehiculos',
+            },
+            {
+                nombre: 'clientes',
+            },
+            {
+                nombre: 'compañias',
+            },
+            {
+                nombre: 'privilegios',
+            },
+            {
+                nombre: 'permisos',
+            },
+            {
+                nombre: 'roles',
+            },
+            {
+                nombre: 'tipos',
+            },
+            {
+                nombre: 'viajes',
+            },
+            {
+                nombre: 'cotizaciones',
+            },
+        ]);
 
         const role = await Role.create({
-            nombre: "Administrador",
-            permissionsId: [1]
+            nombre: 'conductor',
+            permissionsId: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            privilegesId: [1, 2, 3],
         });
+
+        await role.save();
 
         const company = await Company.create({
             nit: 1010101010,
@@ -50,23 +91,27 @@ const seedDB = async(req, res) => {
             duenoPoliza: "yo"
         });
 
-        const encryptedPass = bcrypt.hashSync(123456, 10);
+        await company.save();
+
+        const client = await Client.create({
+            documento: 1010101010,
+            nombre: 'Jefferson',
+            razonSocial: 'si',
+            telefono: 3333333333
+        });
+
+        const encryptedPass = bcrypt.hashSync('123456', 10);
 
         const user = await User.create({
             documento: 1010101010,
             edad: 12,
-            email: "carti@gmail.com",
+            email: 'carti@gmail.com',
             contrasena: encryptedPass,
             roleId: 1,
             companyId: 1,
             vehicleId: 1
         });
 
-        await vehicleType.save();
-        await vehicle.save();
-        await permission.save();
-        await role.save();
-        await company.save();
         await user.save();
 
         res.status(200).json({
